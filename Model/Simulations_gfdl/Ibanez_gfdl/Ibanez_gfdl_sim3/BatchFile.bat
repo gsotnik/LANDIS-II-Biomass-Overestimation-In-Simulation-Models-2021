c8ecb9c20c99d04a8cb63824759d20104acc644d
Garry Sotnik <gsotnik@gmail.com>
2021-05-14 16:33:13 -0400

    update

---
+++ Model/Simulations_gfdl/Ibanez_gfdl/Ibanez_gfdl_sim3/BatchFile.bat
@@ -0,0 +1,7 @@
+rem Batch File to Run a Scenario 
+rem The 'rem' keyword indicates that this is a remark
+
+call landis-ii-7 Ibanez_higher_em_scenario.txt
+
+pause
+
---
+++ Model/Simulations_pcm/Ibanez_pcm/Ibanez_pcm_sim3/BatchFile.bat
@@ -0,0 +1,7 @@
+rem Batch File to Run a Scenario 
+rem The 'rem' keyword indicates that this is a remark
+
+call landis-ii-7 Ibanez_lower_em_scenario.txt
+
+pause
+
